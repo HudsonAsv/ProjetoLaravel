79459f20356319ee217e3b0ff276bb15d8996bdf
HudsonAsv <hudsonabraaos@gmail.com>
2025-07-04 21:26:30 -0400

    Bloqueio de mensagens funcionando

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comentario;
+use Illuminate\Support\Facades\Http;
 
 class ComentarioController extends Controller
 {
@@ -14,6 +15,23 @@ class ComentarioController extends Controller
             'autor' => 'required|string|max:100',
             'mensagem' => 'required|string|max:1000',
         ]);
+
+        // Chamada à Perspective API
+        
+$response = Http::withOptions([
+    'verify' => false, // Desativa verificação do certificado SSL
+])->post("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=" . env('PERSPECTIVE_API_KEY'), [
+    "comment" => ["text" => $request->mensagem],
+    "requestedAttributes" => ["TOXICITY" => new \stdClass()],
+]);
+
+        // Extrai o score de toxicidade
+        $score = $response['attributeScores']['TOXICITY']['summaryScore']['value'] ?? 0;
+
+        // Bloqueia se score for alto
+        if ($score >= 0.7) {
+            return back()->withErrors(['mensagem' => 'Comentário considerado ofensivo e foi bloqueado.']);
+        }
 
         Comentario::create([
             'ocorrencia_id' => $request->ocorrencia_id,
